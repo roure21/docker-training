509b7d83dc9feb7df17e2f3b8d9ba51f460ab92c
Toni Robles <r21robles@gmail.com>
2019-12-11 10:47:23 +0100

    Paso 8: PostgreSQL

---
+++ elmanipulador/backup.sql
@@ -0,0 +1,26 @@
+BEGIN TRANSACTION;
+INSERT INTO "auth_user" VALUES(1,'pbkdf2_sha256$180000$lFnP99kleX4e$FrGQf2aW3IrkfUZ8Ac3vDYpmGETJ4VoivsIuq3xDImo=',NULL,TRUE,'admin','Administrador','','',TRUE,TRUE,'2019-12-09 11:47:54');
+INSERT INTO "author" VALUES(1,'Paco','Trampa');
+INSERT INTO "author" VALUES(2,'Fernando','Mentira');
+INSERT INTO "article" VALUES(1,'Vigo compra una central nuclear de segunda mano para suministrar energía al alumbrado navideño','Aunque queda casi un mes para el día de Navidad, Vigo ha tenido que comprar una central nuclear de segunda mano en Milanuncios para poder seguir suministrando energía a su alumbrado navideño. “Algunas luces empezaban a apagarse porque la energía de la que disponemos en la ciudad es insuficiente”, reconoce el alcalde Abel Caballero.
+
+Después de buscar detenidamente entre las diferentes ofertas, finalmente Vigo le ha comprado la central nuclear a Cáceres, que la ofrecía en Milanuncios porque ya tenía dos. La negociación ha sido larga pero al final ha llegado a buen puerto. El pasado viernes, Abel Caballero se desplazó hasta Extremadura para probar la central y, al ver que funcionaba perfectamente, esta mañana una expedición viguesa ha ido a recogerla.
+
+La central nuclear se ha instalado en medio de una rotonda situada en el barrio de Coya para que, además de dar energía, sirva de decoración. Desde su adquisición, la noria gigante que se ha colocado en la ciudad gira a toda velocidad y ya son muchos los vigueses que han salido disparados hacia la ría entre carcajadas. “Confiamos que, con esta central nuclear, podamos sobrevivir al menos hasta Reyes”, se felicitan desde el Ayuntamiento.
+
+En cuanto acabe la Navidad, Vigo intentará vender la central nuclear a alguna ciudad que, por lo que sea, necesite encender millones de luces durante el mes de febrero. “Nosotros pondremos un anuncio y seguro que hay alguna ciudad interesada”, asegura Abel Caballero.','2019-12-09',2);
+INSERT INTO "article" VALUES(2,'Compra unas canicas en Milanuncios con la esperanza de que sean las del vecino de arriba','Después de innumerables noches sin dormir, Concha Fajardo, una vecina del centro de Madrid, ha optado por comprar unas canicas en con la esperanza de que sean las de su vecino de arriba. Esta mujer lleva años desvelándose de madrugada por culpa del inquietante sonido de unas canicas rodando por el suelo del piso de arriba. “Ya lo he probado todo, esta es mi última opción”, reconoce totalmente desesperada.
+
+Fajardo ha utilizado tapones, música y hasta ha recubierto el techo con hueveras, pero el sonido de las canicas jamás ha cesado. “Siempre a la misma hora, entre las dos y las cuatro de la madrugada, mi vecino empieza a hacer rodar sus canicas”, asegura. “Que las utilice a esas horas me hace pensar que juega con ellas a escondidas, así que no me parece que sea del todo descabellado que se quiera deshacer de ellas”, añade con cierta ilusión.
+
+Tras adquirir el juego de canicas por apenas cinco euros, Concha se metió en la cama ayer con la esperanza de poder dormir de una vez por todas del tirón, pero a las dos de la mañana el sonido del cristal rodando sobre la madera volvió a oírse. “Al menos ahora puedo llenar mis noches de insomnio entreteniéndome con mis canicas”, se sincera.
+
+Esta no es la primera vez que Concha fracasa en un intento de este tipo: en 2017, la mujer ya se compró más de 50 taladros con la esperanza de que alguno fuera el del vecino que siempre hace agujeros los domingos por la mañana.','2019-12-02',1);
+INSERT INTO "article" VALUES(3,'Miles de aves migratorias chocan con un muro construido por otras aves que no son migratorias','Tragedia a dos mil metros de altitud. Una enorme bandada de aves migratorias que se dirigía, como cada año, de los trópicos al norte, ha chocado con un muro construido por otras aves que no son migratorias. El suelo se ha llenado de plumas y la situación en la frontera es dramática.
+
+Las aves no migratorias estaban hartas de que las aves migratorias pasaran por su territorio porque consideraban que traían violencia y enfermedades, además de tratarse de especies exóticas que no despiertan confianza. Las aves migratorias se han visto obligadas a volver a los trópicos, donde en esta época del año no hay comida, lo que puede provocar un importante desequilibrio en la diversidad de la zona.
+
+Los huevos de las aves migratorias, al ser menores, han sido encerrados en pequeños nidos a la espera de que las aves no migratorias decidan qué hacer con ellos. De momento no les van a dar calor y tampoco van a dejar que entren en contacto con sus huevos por miedo a que les hagan algo.
+
+Aunque el muro es robusto e infranqueable, muchas aves viajeras del norte sí están pudiendo ir a los trópicos para, durante unos días, ver cómo viven las aves migratorias y así crecer interiormente.','2019-12-18',2);
+COMMIT;
--- elmanipulador/config/settings.py
+++ elmanipulador/config/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
